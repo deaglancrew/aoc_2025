bc945069906862d17546216e11b062aa19c111e6
Deaglan Crew <decdexter@googlemail.com>
2025-12-02 17:00:13 +0000

    Generalise and solve part 2

--- day2.py
+++ day2.py
@@ -10,13 +10,20 @@ class IdRange(NamedTuple):
         return cls(int(start), int(end))
 
     def starting_palindrome(self):
-        return PalindromicNumber.first_palindrome_after(self.start)
+        return MultiNumber.first_multi_after(self.start)
 
     def all_matching_palindromes(self):
         palindrome = self.starting_palindrome()
         while palindrome.value <= self.end:
             yield palindrome
-            palindrome = palindrome.next_palindrome()
+            palindrome = palindrome.next_number()
+
+    def all_matching_multi_numbers(self):
+        for i in range(2, len(str(self.end)) + 1):
+            number = MultiNumber.first_multi_after(self.start, i)
+            while number.value <= self.end:
+                yield number
+                number = number.next_number()
 
     @classmethod
     def parse_ranges(cls, ranges: str):
@@ -25,41 +32,40 @@ class IdRange(NamedTuple):
             yield cls.from_string(r)
         
 
-class PalindromicNumber:
-    def __init__(self, value: int):
-        self._value = value
-        str_base = str(value)
-        self._base_str = list(str_base)
-
-    @property
-    def left(self):
-        return ''.join(self._base_str[:len(self._base_str) // 2])
+class MultiNumber:
+    def __init__(self, base_value: int, replica_count: int = 2):
+        self._base_value = base_value
+        str_base = str(base_value)
+        self._value = int(str_base * replica_count)
+        self._replica_count = replica_count
 
     @property
     def value(self):
         return self._value
     
-    def next_palindrome(self):
-        return PalindromicNumber(int(str(int(self.left) + 1) * 2))
-
-    @classmethod
-    def first_palindrome_after(cls, number: int):
-        str_start = str(number)
-        if len(str_start) == 1:
-            palindrome = PalindromicNumber(11)
-        else:
-            if len(str_start) % 2 == 1:
-                str_start = '0' + str_start
-            midpoint = len(str_start) // 2
-            palindrome = cls(int(str_start[:midpoint] * 2))
-        while True:
-            if (len(str(palindrome.value)) % 2 == 0) and palindrome.value >= number:
-                break
-            palindrome = palindrome.next_palindrome()
-        return palindrome
+    def next_number(self):
+        return MultiNumber(self._base_value + 1, self._replica_count)
 
     def __repr__(self):
-        return f'PalindromicNumber(value={self.value})'
+        return f'MultiNumber(value={self.value}, replica_count={self._replica_count})'
+
+    @classmethod
+    def first_multi_after(cls, number: int, replica_count: int = 2):
+        str_start = str(number)
+        if len(str_start) <= replica_count:
+            palindrome = cls(1, replica_count)
+        else:
+            if len(str_start) % replica_count != 0:
+                palindrome = cls(int('1' + ('0' * (len(str_start) // replica_count - 1))), replica_count)
+            else:
+                midpoint = len(str_start) // replica_count
+                palindrome = cls(int(str_start[:midpoint]), replica_count)
+        while True:
+            if (len(str(palindrome.value)) % replica_count == 0) and palindrome.value >= number:
+                break
+            palindrome = palindrome.next_number()
+        return palindrome
+
 
 def solve_1(ranges: str):
     invalid_ids = set()
@@ -74,3 +80,16 @@ assert solve_1(EXAMPLE_IN) == 1227775554
 
 with open('inputs/day2.txt') as infile:
     print('Part 1 solution:', solve_1(infile.read()))
+
+def solve_2(ranges: str):
+    invalid_ids = set()
+    for r in IdRange.parse_ranges(ranges):
+        for palindrome in r.all_matching_multi_numbers():
+            invalid_ids.add(palindrome.value)
+    return sum(invalid_ids)
+
+assert solve_2(EXAMPLE_IN) == 4174379265
+
+
+with open('inputs/day2.txt') as infile:
+    print('Part 2 solution:', solve_2(infile.read()))
